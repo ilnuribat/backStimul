5e28fabed937864c6c19a6cbba3b6953b473965e
Tolstobrov Alexey <dkn2002@mail.ru>
2018-09-21 13:34:15 +0300

    edit resolvers

--- server/data/resolvers/user.js
+++ server/data/resolvers/user.js
@@ -1,38 +1,85 @@
-const _ = require('lodash');
-const models = require('./../models');
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
-const { sequelize: { Op } } = models;
+import { userLogic } from './logic';
+import { JWT_SECRET } from '../config';
+import { User } from './connectors';
 
 module.exports = {
   User: {
-    projects: async (parent) => {
-      const userProjects = await models.ProjectUser.findAll({ where: { userId: parent.id } });
-
-      return models.Project.findAll({
-        where: {
-          id: {
-            [Op.in]: _.map(userProjects, 'projectId'),
-          },
-        },
-      });
+    email(user, args, ctx) {
+      return userLogic.email(user, args, ctx);
+    },
+    friends(user, args, ctx) {
+      return userLogic.friends(user, args, ctx);
+    },
+    groups(user, args, ctx) {
+      return userLogic.groups(user, args, ctx);
+    },
+    jwt(user, args, ctx) {
+      return userLogic.jwt(user, args, ctx);
+    },
+    messages(user, args, ctx) {
+      return userLogic.messages(user, args, ctx);
     },
   },
-
   Query: {
-    user: (parent, { id }) => models.User.findOne({ where: { id } }),
-    users: async () => {
-      const where = { [Op.and]: [] };
-
-      return models.User.findAll({ where });
+    user(_, args, ctx) {
+      return userLogic.query(_, args, ctx);
     },
   },
 
   Mutation: {
-    updateUser: async (parent, args) => models.User.update(
-      args,
-      {
-        where: { id: args.id },
-      },
-    ),
+    login(_, signinUserInput, ctx) {
+      // find user by email
+      const { email, password } = signinUserInput.user;
+
+      return User.findOne({ where: { email } }).then((user) => {
+        if (user) {
+          // validate password
+          return bcrypt.compare(password, user.password).then((res) => {
+            if (res) {
+              // create jwt
+              const token = jwt.sign({
+                id: user.id,
+                email: user.email,
+                version: user.version,
+              }, JWT_SECRET);
+              user.jwt = token;
+              ctx.user = Promise.resolve(user);
+              return user;
+            }
+
+            return Promise.reject('password incorrect');
+          });
+        }
+
+        return Promise.reject('email not found');
+      });
+    },
+    signup(_, signinUserInput, ctx) {
+      const { email, password, username } = signinUserInput.user;
+
+      // find user by email
+      return User.findOne({ where: { email } }).then((existing) => {
+        if (!existing) {
+          // hash password and create user
+          return bcrypt.hash(password, 10).then(hash => User.create({
+            email,
+            password: hash,
+            username: username || email,
+            version: 1,
+          })).then((user) => {
+            const { id } = user;
+            const token = jwt.sign({ id, email, version: 1 }, JWT_SECRET);
+            user.jwt = token;
+            ctx.user = Promise.resolve(user);
+            return user;
+          });
+        }
+
+        return Promise.reject('email already exists'); // email already exists
+      });
+    },
   },
 };
