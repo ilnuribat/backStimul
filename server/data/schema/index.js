714f8bbd255ffb5e6a2db5a40018f52bddc37195
Tolstobrov Alexey <dkn2002@mail.ru>
2018-09-21 15:09:24 +0300

    fixes

--- server/data/schema/index.js
+++ server/data/schema/index.js
@@ -4,20 +4,24 @@ const { mergeTypes } = require('merge-graphql-schemas');
 const settings = { encoding: 'utf8' };
 
 const userTypes = readFileSync(`${__dirname}/user.graphql`, settings);
-const usersTypes = readFileSync(`${__dirname}/users.graphql`, settings);
+// const usersTypes = readFileSync(`${__dirname}/users.graphql`, settings);
 const projectTypes = readFileSync(`${__dirname}/project.graphql`, settings);
 const columnTypes = readFileSync(`${__dirname}/column.graphql`, settings);
 const taskTypes = readFileSync(`${__dirname}/task.graphql`, settings);
 const projectGroupTypes = readFileSync(`${__dirname}/projectGroup.graphql`, settings);
 const glossaryTypes = readFileSync(`${__dirname}/glossary.graphql`, settings);
+const messageTypes = readFileSync(`${__dirname}/message.graphql`, settings);
+const groupmessageTypes = readFileSync(`${__dirname}/groupmessageTypes.graphql`, settings);
 
 
 module.exports = mergeTypes([
   userTypes,
-  usersTypes,
+  // usersTypes,
   projectTypes,
   columnTypes,
   taskTypes,
   projectGroupTypes,
   glossaryTypes,
+  messageTypes,
+  groupmessageTypes,
 ]);
