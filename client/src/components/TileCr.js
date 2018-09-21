5f447c288240dcad234546b534e8a0b9b0822b61
godlatro <godlatro1@gmail.com>
2018-09-21 14:00:33 +0300

    Ani

--- client/src/components/TileCr.js
+++ client/src/components/TileCr.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { quf } from '../constants';
-
+import 'animate.css';
 
 const crPrGr = (parentId, name, createdBy) => (
   `mutation{
@@ -118,7 +118,7 @@ export default class CrTile extends Component {
       );
     }else{
       return(
-        <div className="tile" onClick={this._open}>
+        <div className="tile animated fadeInLeftBig" onClick={this._open}>
           <svg className="svg-add" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path d="M0 0h24v24H0z" fill="none" />
             <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
