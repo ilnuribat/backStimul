4ca73fd9695809e07415bbfec187405b26ba73f1
ilnuribat <ilnuribat@gmail.com>
2018-09-24 19:00:23 +0300

    hide all extra models. only required

--- server/data/models/index.js
+++ server/data/models/index.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-const columnModel = require('./column');
-const projectModel = require('./project');
-const taskSchema = require('./task');
+// const columnModel = require('./column');
+// const projectModel = require('./project');
+// const taskSchema = require('./task');
 const userModel = require('./user');
-const projectGroupModel = require('./projectGroup');
+// const projectGroupModel = require('./projectGroup');
 
 const db = {
-  Column: mongoose.model('Column', columnModel),
-  Project: mongoose.model('Project', projectModel),
-  ProjectGroup: mongoose.model('ProjectGroup', projectGroupModel),
-  Task: mongoose.model('Task', taskSchema),
-  User: mongoose.model('User', userModel),
+  // Column: mongoose.model('Column', columnModel),
+  // Project: mongoose.model('Project', projectModel),
+  // ProjectGroup: mongoose.model('ProjectGroup', projectGroupModel),
+  // Task: mongoose.model('Task', taskSchema),
+  User: mongoose.model('users', userModel),
 };
 
 module.exports = db;
