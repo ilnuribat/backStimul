b135467dc6b5b0ee0b67b9159009cac355e6a6fb
ilnuribat <ilnuribat@gmail.com>
2018-09-25 15:06:43 +0300

    only on resolver and only one schema

--- server/data/resolvers/index.js
+++ server/data/resolvers/index.js
@@ -1,16 +1,14 @@
 const { mergeResolvers } = require('merge-graphql-schemas');
 const userResolvers = require('./user');
-// const usersResolvers = require('./users');
-const projectResolvers = require('./project');
-const columnResolvers = require('./column');
-const taskResolvers = require('./task');
-const projectGroupsResolvers = require('./projectGroup');
+// const projectResolvers = require('./project');
+// const columnResolvers = require('./column');
+// const taskResolvers = require('./task');
+// const projectGroupsResolvers = require('./projectGroup');
 
 module.exports = mergeResolvers([
   userResolvers,
-  // usersResolvers,
-  projectResolvers,
-  columnResolvers,
-  taskResolvers,
-  projectGroupsResolvers,
+  // projectResolvers,
+  // columnResolvers,
+  // taskResolvers,
+  // projectGroupsResolvers,
 ]);
--- server/data/schema/index.js
+++ server/data/schema/index.js
@@ -1,25 +1,25 @@
 const { readFileSync } = require('fs');
 const { mergeTypes } = require('merge-graphql-schemas');
 
-const settings = { encoding: 'utf8' };
+function getSchema(name) {
+  return readFileSync(`${__dirname}/${name}.graphql`, { encoding: 'utf8' });
+}
 
-const userTypes = readFileSync(`${__dirname}/user.graphql`, settings);
-// const usersTypes = readFileSync(`${__dirname}/users.graphql`, settings);
-const projectTypes = readFileSync(`${__dirname}/project.graphql`, settings);
-const columnTypes = readFileSync(`${__dirname}/column.graphql`, settings);
-const taskTypes = readFileSync(`${__dirname}/task.graphql`, settings);
-const projectGroupTypes = readFileSync(`${__dirname}/projectGroup.graphql`, settings);
-const messageTypes = readFileSync(`${__dirname}/message.graphql`, settings);
-const groupmessageTypes = readFileSync(`${__dirname}/groupmessageTypes.graphql`, settings);
+const userTypes = getSchema('user');
+// const projectTypes = getSchema('project');
+// const columnTypes = getSchema('column');
+// const taskTypes = getSchema('task');
+// const projectGroupTypes = getSchema('projectGroup');
+// const messageTypes = getSchema('message');
+// const groupmessageTypes = getSchema('groupmessageTypes');
 
 
 module.exports = mergeTypes([
   userTypes,
-  // usersTypes,
-  projectTypes,
-  columnTypes,
-  taskTypes,
-  projectGroupTypes,
-  messageTypes,
-  groupmessageTypes,
+  // projectTypes,
+  // columnTypes,
+  // taskTypes,
+  // projectGroupTypes,
+  // messageTypes,
+  // groupmessageTypes,
 ]);
