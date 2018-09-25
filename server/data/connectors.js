56148861360170c8cfccb06b21ac53c3400c1804
Tolstobrov Alexey <dkn2002@mail.ru>
2018-09-25 18:26:51 +0300

    eslint + pg support

--- server/data/connectors.js
+++ server/data/connectors.js
@@ -10,6 +10,15 @@ const db = new Sequelize('chatty', null, null, {
   logging: false, // mark this true if you want to see logs
 });
 
+
+// // initialize our database
+// const db = new Sequelize('db', 'a', '1', {
+//   dialect: 'postgres',
+//   // storage: './chatty.sqlite',
+//   logging: false, // mark this true if you want to see logs
+// });
+
+
 // define groups
 const GroupModel = db.define('group', {
   name: { type: Sequelize.STRING },
@@ -48,42 +57,43 @@ const GROUPS = 4;
 const USERS_PER_GROUP = 5;
 const MESSAGES_PER_USER = 5;
 faker.seed(1); // get consistent data every time we reload app
-
+// db.sync({ force: true });
 // you don't need to stare at this code too hard
 // just trust that it fakes a bunch of groups, users, and messages
-// db.sync({ force: true }).then(() => _.times(GROUPS, () => GroupModel.create({
-//   name: faker.lorem.words(3),
-// }).then(group => _.times(USERS_PER_GROUP, () => {
-//   const password = faker.internet.password();
-//   return bcrypt.hash(password, 10).then(hash => group.createUser({
-//     email: faker.internet.email(),
-//     username: faker.internet.userName(),
-//     password: hash,
-//     version: 1,
-//   }).then((user) => {
-//     console.log(
-//       '{email, username, password}',
-//       `{${user.email}, ${user.username}, ${password}}`
-//     );
-//     _.times(MESSAGES_PER_USER, () => MessageModel.create({
-//       userId: user.id,
-//       groupId: group.id,
-//       text: faker.lorem.sentences(3),
-//     }));
-//     return user;
-//   }));
-// })).then((userPromises) => {
-//    // make users friends with all users in the group
-//   Promise.all(userPromises).then((users) => {
-//     _.each(users, (current, i) => {
-//       _.each(users, (user, j) => {
-//         if (i !== j) {
-//           current.addFriend(user);
-//         }
-//       });
-//     });
-//   });
-// })));
+
+db.sync({ force: true }).then(() => _.times(GROUPS, () => GroupModel.create({
+  name: faker.lorem.words(3),
+}).then(group => _.times(USERS_PER_GROUP, () => {
+  const password = faker.internet.password();
+  return bcrypt.hash(password, 10).then(hash => group.createUser({
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: hash,
+    version: 1,
+  }).then((user) => {
+    console.log(
+      '{email, username, password}',
+      `{${user.email}, ${user.username}, ${password}}`
+    );
+    _.times(MESSAGES_PER_USER, () => MessageModel.create({
+      userId: user.id,
+      groupId: group.id,
+      text: faker.lorem.sentences(3),
+    }));
+    return user;
+  }));
+})).then((userPromises) => {
+   // make users friends with all users in the group
+  Promise.all(userPromises).then((users) => {
+    _.each(users, (current, i) => {
+      _.each(users, (user, j) => {
+        if (i !== j) {
+          current.addFriend(user);
+        }
+      });
+    });
+  });
+})));
 
 const Group = db.models.group;
 const Message = db.models.message;
