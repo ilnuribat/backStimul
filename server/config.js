af4106742725315d69456b365f123417e7f05b8a
ilnuribat <ilnuribat@gmail.com>
2018-09-27 15:03:56 +0300

    default mongo connection

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const {
   HTTP_PORT = 8500,
   JWT_SECRET = 'super secret',
-  MONGODB_HOST = 'mongodb://127.0.0.1/guov',
+  MONGODB_HOST = 'MONGODB_HOST=mongodb://guov:guov@127.0.0.1:27017/guov?authSource=admin',
   NODE_ENV = 'dev',
   SOCKET_PORT = 4080,
   MICROSERVICES = '',
