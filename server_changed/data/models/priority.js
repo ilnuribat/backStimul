faa2fffe5324cf9d5d518e0bdeb6bcf45b548b09
ilnuribat <ilnuribat@gmail.com>
2018-09-27 12:35:35 +0300

    removed all servers

--- server_changed/data/models/priority.js
+++
@@ -1,6 +0,0 @@
-const { Schema } = require('mongoose');
-
-module.exports = new Schema({
-    id: Number,
-    title: String,
-});
--- server_our_back/src/utils/createMigration.js
+++
@@ -1,8 +0,0 @@
-const { readFileSync, writeFileSync } = require('fs');
-const moment = require('moment');
-
-const example = readFileSync('./migrations/_example.migration', { encoding: 'utf8' });
-const date = moment().format('YYYY-MM-DDTHHmmss');
-
-
-writeFileSync(`./migrations/${date}-unnamed-migration.js`, example);
