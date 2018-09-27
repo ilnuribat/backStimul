0224ceafefed21bfbe5e67a40f8e86d013a7bba2
ilnuribat <ilnuribat@gmail.com>
2018-09-27 11:05:48 +0300

    auth like http

--- client/src/index.js
+++ client/src/index.js
@@ -66,7 +66,7 @@ const wsLink = new WebSocketLink({
     // lazy: true,
     reconnect: true,
     connectionParams() {
-      return { jwt: localStorage.getItem('auth-token') };
+      return { Authorization: `Bearer ${localStorage.getItem('auth-token')}` };
     },
     jwt: localStorage.getItem('auth-token'),
   }
