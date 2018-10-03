8d1c2efa04fbb32bef8a38ee76b3d0900946993d
ilnuribat <ilnuribat@gmail.com>
2018-10-03 14:59:53 +0300

    extra fields

--- server/data/models/user.js
+++ server/data/models/user.js
@@ -3,16 +3,7 @@ const { Schema } = require('mongoose');
 const user = new Schema({
   email: { type: String, unique: true },
   password: String,
-  firstname: String,
-  lastname: String,
   name: String,
-  description: String,
-  pinkey: String,
-  groupId: { type: String, default: 'none' },
-  createdBy: String,
-  deletedAt: Date,
-  hide: { type: Boolean, default: false },
-  type: { type: String, default: 'User' },
 }, {
   timestamps: {
     createdAt: 'createdAt',
