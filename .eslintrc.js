f858b08efb9d8add3ebe8272d712856779ff5bdd
ilnuribat <ilnuribat@gmail.com>
2018-10-04 17:25:01 +0300

    allow only _id

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,6 @@ module.exports = {
     ],
     "func-names": ["off"],
     "class-methods-use-this": ["off"],
-    "no-underscore-dangle": ["off"]
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }]
   }
 };
