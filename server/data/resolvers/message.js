66c2dfcd2bfd9558e7344c018199a912011c7830
ilnuribat <ilnuribat@gmail.com>
2018-10-05 17:17:19 +0300

    messageRead subscriptions

--- server/data/resolvers/message.js
+++ server/data/resolvers/message.js
@@ -6,6 +6,7 @@ const {
 
 const pubsub = new PubSub();
 const MESSAGED_ADDED = 'MESSAGED_ADDED';
+const MESSAGE_READ = 'MESSAGE_READ';
 
 module.exports = {
   Message: {
@@ -21,11 +22,12 @@ module.exports = {
     },
     createdAt: message => moment(message.createdAt).format(),
     userId: message => message.userId.toString(),
-    isRead: async (message, args, { user }) => {
+    isRead: async (message, args, ctx) => {
+      const { user } = ctx;
+
       if (message.isRead !== undefined) {
         return message.isRead;
       }
-      console.log('каждый раз');
       if (message.userId.toString() !== user.id) {
         return true;
       }
@@ -35,7 +37,7 @@ module.exports = {
           $ne: user.id,
         },
         groupId: message.groupId,
-      });
+      }).sort({ _id: 1 });
 
       const unReadMessages = userGroups.filter(ug => ug.lastReadCursor < message.id);
 
@@ -65,7 +67,6 @@ module.exports = {
       const createdMessage = await Message.create({
         userId: user.id,
         ...message,
-        isRead: false,
       });
 
       await UserGroup.update({
@@ -91,6 +92,18 @@ module.exports = {
         throw new Error('no message found');
       }
 
+      // Вытащить последний чужой курсор.
+      const lastAnotherReadCursor = await UserGroup.findOne({
+        groupId: message.groupId,
+        userId: {
+          $ne: user.id,
+        },
+      }).sort({ _id: 1 });
+      const myOldCursor = await UserGroup.findOne({
+        groupId: message.groupId,
+        userId: user.id,
+      });
+
       const userGroup = await UserGroup.updateOne({
         userId: user.id,
         groupId: message.groupId,
@@ -100,7 +113,32 @@ module.exports = {
         },
       });
 
+      // диалог с самим собой, просто выходим
+      if (!lastAnotherReadCursor) {
+        return true;
+      }
+
+      // Если мой курсор был старее всех остальных
+      // значит все сообщения в этом отрезке должны быть прочитаны.
+      if (myOldCursor.lastReadCursor < lastAnotherReadCursor.lastReadCursor) {
+        const endCursor = lastAnotherReadCursor.lastAnotherReadCursor < message._id
+          ? lastAnotherReadCursor.lastReadCursor
+          : message._id;
+        const messages = await Message.find({
+          _id: {
+            $gt: myOldCursor.lastReadCursor,
+            $lte: endCursor,
+          },
+          groupId: message.groupId,
+          userId: { $ne: user.id },
+        });
+
+        messages.map(m => pubsub.publish(MESSAGE_READ, { messageRead: { isRead: true, id: m._id.toString() } }));
+      }
+
       return userGroup.nModified;
+
+      // return false;
     },
   },
   Subscription: {
@@ -108,6 +146,12 @@ module.exports = {
       subscribe: withFilter(
         () => pubsub.asyncIterator([MESSAGED_ADDED]),
         ({ messageAdded: { groupId: mGroupId } }, { groupId }) => mGroupId.toString() === groupId,
+      ),
+    },
+    messageRead: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator([MESSAGE_READ]),
+        ({ messageRead: { id: mId } }, { id }) => mId === id,
       ),
     },
   },
