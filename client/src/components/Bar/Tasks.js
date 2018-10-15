9e9c00f0ea31617f53c37c99d1df90cfb5f6ca10
godlatro <godlatro1@gmail.com>
2018-10-15 15:06:30 +0300

    Task bar groups list

--- client/src/components/Bar/Tasks.js
+++ client/src/components/Bar/Tasks.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { graphql, compose } from 'react-apollo';
 import { PropTypes } from 'prop-types';
-import { setPrivateChat, getPrivateChat, createGroup, user, getCUser } from '../../graph/querys';
-import { qauf, _url } from '../../constants'
+import { setPrivateChat, getPrivateChat, createGroup, user, getCUser, glossaryStatus } from '../../graph/querys';
+import { qauf, _url } from '../../constants';
+import _ from 'lodash';
 import 'animate.css';
+import Loading from '../Loading';
 
 class Tasks extends Component {
   constructor(props) {
@@ -13,6 +15,7 @@ class Tasks extends Component {
       grid: '',
       addGroupInputs: false,
       newGrName: '',
+      status: [],
     }
 
     this.changeGroup = this.changeGroup.bind(this);
@@ -24,7 +27,8 @@ class Tasks extends Component {
   }
 
   componentDidMount(){
-    this.fetcher()
+    this.glossStatus();
+    this.fetcher();
 
   }
 
@@ -69,6 +73,19 @@ class Tasks extends Component {
     })
   }
 
+  glossStatus(){
+
+    qauf(glossaryStatus(), _url, localStorage.getItem('auth-token')).then(a=>{
+      this.setState({
+        status: ["",...a.data.glossary.taskStatuses]
+      });
+      // console.log(a)
+    })
+      .catch((e)=>{
+        console.warn(e);
+      });
+  }
+
   inputChange(e){
     let val = e.target.value;
 
@@ -106,22 +123,76 @@ class Tasks extends Component {
   }
 
   render() {
-
-    const {grl, grid, addGroupInputs, newGrName} = this.state;
+    const {grl, grid, addGroupInputs, newGrName, status} = this.state;
     const { getPrivateChat, getCUser } = this.props;
+
+    let cols = [[],[],[],[],[],[],[]]; 
+
+    if(getCUser.loading) return <Loading />;
+    if(!getCUser.user) return <Loading />;
+    if(!getCUser.user.groups) return <Loading />;
+
+    let arr = getCUser.user.groups;
+
+    arr = _.sortBy(arr, 'unreadCount');
+  
+    _.forEach(arr, (result)=>{
+      if(!result.status){
+        cols[1].push(result);
+      }
+      if(result.status){
+        cols[result.status].push(result);
+      }
+    });
+
+    // return(
+    //   <div className="content-aft-nav columns-wrapper">
+    //     {
+    //       status.map((e,i)=>{
+    //         if(!e.name){
+    //           return true;
+    //         }
+    //         return <Column key={"column"+e.id} name={e.name} tasks={cols[i]} selectTask={this.selectTask} />
+    //       })
+    //     }
+    //   </div>
+    // )
 
     return (
       <div>
         <h3>Задачи</h3>
         <div className='list-container'>
+          {
+            status.map((e,i)=>{
+              if(!e.name){
+                return true;
+              }
 
-          
+              return(
+                <div className="colName">
+                  <div className="colHead">{e.name}</div>
 
-          {getCUser && getCUser.user && getCUser.user.groups.map((e,i)=>{
-            return(
-              <div key={"gr"+i} role="presentation" className={grid === e.id || getPrivateChat.id === e.id ? 'active list animated fadeIn' : 'list animated fadeIn'} onClick={()=>{this.changeGroup(e.id, e.name)}}>{e.name}</div>
-            )
-          }) }
+                  {cols[i].map((e,i)=>{
+                      return(
+                        <div key={"gr"+i} role="presentation" className={grid === e.id || getPrivateChat.id === e.id ? 'active list animated fadeIn' : 'list animated fadeIn'} onClick={()=>{this.changeGroup(e.id, e.name)}}>{e.name}</div>
+                      )
+                    })
+                  }
+                </div>
+              )
+
+
+            })
+          }
+
+                  {/* {arr.map((e,i)=>{
+                      return(
+                        <div key={"gr"+i} role="presentation" className={grid === e.id || getPrivateChat.id === e.id ? 'active list animated fadeIn' : 'list animated fadeIn'} onClick={()=>{this.changeGroup(e.id, e.name)}}>{e.name}</div>
+                      )
+                    })
+                  } */}
+
+
           {
             addGroupInputs?(
               <div className="list animated fadeInLeft" style={{textAlign: "center"}}>
