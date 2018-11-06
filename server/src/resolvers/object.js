db6fe9211361b10e3a697714aa9d2d66587c7748
ilnur ibatullin <ilnuribat@gmail.com>
2018-11-06 14:25:23 +0300

    update object, get all objects, get only objects you have permissions

--- server/src/resolvers/object.js
+++ server/src/resolvers/object.js
@@ -1,12 +1,20 @@
-const { Group } = require('../models');
+const { Group, UserGroup } = require('../models');
 const obejctService = require('../services/object');
 const addressService = require('../services/address');
 
 module.exports = {
   Object: {
-    async tasks(parent) {
+    async tasks(parent, args, { user }) {
+      const userGroups = await UserGroup.find({
+        userId: user.id,
+      });
+
       return Group.find({
         objectId: parent.id,
+        code: null,
+        _id: {
+          $in: userGroups.map(ug => ug.groupId),
+        },
       });
     },
     parentId(parent) {
@@ -28,6 +36,9 @@ module.exports = {
     async object(parent, { id }) {
       return Group.findById(id);
     },
+    objects() {
+      return Group.find({ type: 'OBJECT' });
+    },
   },
   Mutation: {
     async createObject(parent, { object }) {
@@ -43,6 +54,28 @@ module.exports = {
       const res = await Group.create(object);
 
       return res;
+    },
+    async updateObject(parent, { id, object }) {
+      const objectId = id || object.id;
+      const foundObject = await Group.findOne({ _id: objectId, type: 'OBJECT' });
+
+      if (!foundObject) {
+        throw new Error('no object found');
+      }
+
+      if (object.address && object.address !== foundObject.address.value) {
+        const formedAddress = await addressService.formAddress(object.address);
+
+        Object.assign(object, { address: formedAddress });
+      }
+
+      const res = await Group.update({
+        _id: id,
+      }, {
+        $set: object,
+      });
+
+      return res.nModified;
     },
   },
 };
