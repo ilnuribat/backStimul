ea485fb3b7be5ec706623022e442c70112f0f388
Tolstobrov Alexey <dkn2002@mail.ru>
2018-11-09 11:16:58 +0300

    added mimetype

--- server/src/models/files.js
+++ server/src/models/files.js
@@ -1,8 +1,9 @@
 const { Schema } = require('mongoose');
 
 const schema = new Schema({
-  taskId: Schema.Types.String,
-  fileId: Schema.Types.String,
+  taskId: String,
+  fileId: String,
+  mimetype: String,
 });
 
 schema.index({ taskId: 1, fileId: 1 }, { unique: true });
