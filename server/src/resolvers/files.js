9137795f64934df416c14aaf31f6ee270b8e8c5a
Tolstobrov Alexey <dkn2002@mail.ru>
2018-11-09 11:18:23 +0300

    save file, send meta info back to front

--- server/src/resolvers/files.js
+++ server/src/resolvers/files.js
@@ -1,9 +1,10 @@
-const { connection, Types: { ObjectId }, mongo: { GridFSBucket } } = require('mongoose');
+const fs = require('fs');
+const filesize = require('filesize');
+const { connection, mongo: { GridFSBucket } } = require('mongoose');
 const {
   Files,
 } = require('../models');
 // const { GraphQLUpload } = require('apollo-server');
-const fs = require('fs');
 
 const download = async function (id) {
   const bucket = new GridFSBucket(connection.db, { bucketName: 'gridfsdownload' });
@@ -24,70 +25,65 @@ const download = async function (id) {
 };
 
 
-const fileUpload = async (taskId, fileId) => {
-  try {
-    await Files.create({
-      taskId,
-      fileId,
-    });
-  } catch (err) {
-    if (err.errmsg && err.errmsg.indexOf('duplicate key error') > -1) {
-      await Files.findOne({ fileId });
-    }
-  }
-};
+const fileUpload = async ({ taskId, fileId }) => Files.create({
+  taskId,
+  fileId,
+});
 
-const storeUpload = ({ stream, filename, id }) => {
+const storeUpload = ({ stream, filename: name, taskId }) => {
   const bucket = new GridFSBucket(connection.db, { bucketName: 'gridfsdownload' });
-  const uploadStream = bucket.openUploadStream(filename);
+  const uploadStream = bucket.openUploadStream(name);
 
   // download(ObjectId('5bdc50d4d1531833206a7ed0'));
   return new Promise((resolve, reject) => stream
     .pipe(uploadStream)
-    .on('finish', () => {
-      console.log('fuinish');
-      fileUpload(id, uploadStream.id);
+    .on('finish', async () => {
+      await fileUpload({ taskId, fileId: uploadStream.id }).then;
 
-      return uploadStream.id;
+      return resolve({
+        id: uploadStream.id.toString(),
+        name,
+        size: filesize(uploadStream.length),
+      });
     })
     .on('error', reject));
 };
 
 module.exports = {
-  Query: {
-    findFiles: async (parent, { id }) => {
-      const file = await Files.find({ taskId: id });
-
-      const file = Files.aggregate([
-        {$match: { taskId: "5be2d0efe7c10e6642ec1662" }},
-        {$addFields: {fileIdObject: {"$toObjectId": "$fileId"}}},
-        {$lookup:
-          {from: 'gridfsdownload.files',
-            localField: 'fileIdObject',
-            foreignField: '_id',
-            as: 'ururu'
-          }
-        }]);
-
-
-      // .exec((er, doc) => {
-      //   console.log(doc);
-      //   return doc;
-      // });
-      // console.log(ObjectId(file.fileId).toString(), "string");
+  Task: {
+    files: async ({ id }) => {
+      const file = await Files.aggregate([{
+        $match: {
+          taskId: id,
+        },
+      }, {
+        $addFields: {
+          fileIdObject: {
+            $toObjectId: '$fileId',
+          },
+        },
+      }, {
+        $lookup: {
+          from: 'gridfsdownload.files',
+          localField: 'fileIdObject',
+          foreignField: '_id',
+          as: 'fileBody',
+        },
+      }, {
+        $unwind: {
+          path: '$fileBody',
+        },
+      }]);
 
       return file;
     },
   },
-  // Upload: GraphQLUpload,
   Mutation: {
-    async uploadFile(parent, { id, file }) {
-      const { stream, filename, mimetype } = await file;
-      const ida = await storeUpload({ stream, filename, id });
+    async uploadFile(parent, { id: taskId, file }) {
+      const loadedFile = await file;
+      const fileSaved = await storeUpload({ ...loadedFile, taskId });
 
-      // console.log("AAAAAAAAA")
-
-      return { ida, filename, mimetype };
+      return fileSaved;
     },
   },
 };
