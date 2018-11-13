2877ca7bf82a996d05bac38294765fb48e741a33
ilnur ibatullin <ilnuribat@gmail.com>
2018-11-13 19:31:44 +0300

    dont log in test and production stages

--- server/connectDB.js
+++ server/connectDB.js
@@ -15,7 +15,7 @@ module.exports = async function () {
         reject(error);
       });
 
-    mongoose.set('debug', true);
+    mongoose.set('debug', !['test', 'production'].includes(process.env.NODE_ENV));
 
     mongoose.connect(MONGODB_HOST, {
       useNewUrlParser: true,
