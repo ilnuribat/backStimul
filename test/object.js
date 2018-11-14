a567546cb6356c6b3d63c85003d44492ea09ce33
ilnur ibatullin <ilnuribat@gmail.com>
2018-11-14 19:10:10 +0300

    object testing

---
+++ test/object.js
@@ -0,0 +1,38 @@
+const { assert } = require('chai');
+const { Group } = require('../server/src/models');
+
+const query = (name, address) => `
+  mutation {
+    createObject(object: {
+      name: "${name}"
+      address: "${address}"
+    }) {
+      id
+      name
+      address {
+        coordinates
+      }
+    }
+  }
+`;
+
+describe.skip('object', () => {
+  after(async function () {
+    await Group.deleteOne({ _id: this.objectId });
+  });
+  it('create', async function () {
+    const { data, errors } = await this.request({
+      body: { query: query('test', 'г Москва, ул Бакунинская, д 17/28') },
+    });
+
+    assert.isUndefined(errors);
+
+    const object = await Group.findById(data.createObject.id);
+
+    assert.isObject(object);
+    assert.equal(object.name, 'test');
+    assert.equal(object.address.value, 'г Москва, ул Бакунинская, д 17/28');
+
+    this.objectId = object.id;
+  });
+});
