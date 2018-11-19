b66c8f7210dedd01b44f6abed9a05d70bfd326c8
ilnur ibatullin <ilnuribat@gmail.com>
2018-11-19 14:52:36 +0300

    removed useless packages

--- test/init.js
+++ test/init.js
@@ -1,9 +1,5 @@
-const ws = require('ws');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { SubscriptionClient } = require('subscriptions-transport-ws');
-const { ApolloClient } = require('apollo-client');
-const { WebSocketLink } = require('apollo-link-ws');
 const { generateToken } = require('../server/src/services/user');
 const { User } = require('../server/src/models');
 const { server } = require('../server');
@@ -50,28 +46,6 @@ before(async function () {
       .set(headerName, headerValue)
       .then(r => r.body);
   };
-});
-
-// websocket connection
-before(async function () {
-  const GRAPHQL_ENDPOINT = `ws://localhost:${this.httpPort()}/graphql`;
-  const client = new SubscriptionClient(GRAPHQL_ENDPOINT, {
-    reconnect: true,
-  }, ws);
-
-  const apolloClient = new ApolloClient({
-    networkInterface: client,
-    link: new WebSocketLink({
-      client,
-      uri: GRAPHQL_ENDPOINT,
-    }),
-  });
-
-  this.apolloClient = apolloClient;
-});
-
-it.only('ws test', async function () {
-  console.log(this.apolloClient);
 });
 
 after(async function () {
