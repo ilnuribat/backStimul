79c5d9c66bfc91b2a3a2a569e16278acbb6d6536
ilnur ibatullin <ilnuribat@gmail.com>
2018-11-20 13:53:49 +0300

    mock of search

---
+++ server/src/resolvers/search.js
@@ -0,0 +1,24 @@
+module.exports = {
+  Query: {
+    search() {
+      return [{
+        _id: 'asdf1',
+        id: 'asdf2',
+        email: 'Ilnur1',
+        __typename: 'User',
+      }, {
+        __typename: 'Task',
+        id: 'asdf',
+        name: 'new task',
+      }, {
+        __typename: 'Object',
+        id: 'address id',
+        name: 'Ufa',
+      }, {
+        __typename: 'Message',
+        id: 'message id',
+        text: 'some message text',
+      }];
+    },
+  },
+};
