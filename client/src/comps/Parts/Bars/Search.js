ef95ec2f4fa6155cd5e007899f43f35369df5e3b
Tolstobrov Alexey <dkn2002@mail.ru>
2018-11-23 13:28:27 +0300

    Search

--- client/src/comps/Parts/Bars/Search.js
+++ client/src/comps/Parts/Bars/Search.js
@@ -71,7 +71,7 @@ export class Search extends Component {
               <input type="text"  value={value} onChange={this.handleChange} placeholder="Найти задачи, человека, объект..."/>
             </label>
           </form>
-          
+
         </div>
         <div className="SearchBody">
           {
@@ -81,53 +81,80 @@ export class Search extends Component {
                   ({data, loading, error})=>{
                     if(error) {
                       console.log(error)
+
                       return error.message;
                     }
                     if(loading) return "загрузка"
                     if(data && data.previewSearch){
                       let Search={};
 
-                        data.previewSearch.messages && data.previewSearch.messages.length > 0 ? Search.messages = data.previewSearch.messages : null
-                        data.previewSearch.tasks && data.previewSearch.tasks.length > 0 ? Search.tasks = data.previewSearch.tasks : null
-                        data.previewSearch.objects && data.previewSearch.objects.length > 0 ? Search.objects = data.previewSearch.objects : null
-                        data.previewSearch.users && data.previewSearch.users.length > 0 ? Search.users = data.previewSearch.users : null
+                      data.previewSearch.messages && data.previewSearch.messages.length > 0 ? Search.messages = data.previewSearch.messages : null
+                      data.previewSearch.tasks && data.previewSearch.tasks.length > 0 ? Search.tasks = data.previewSearch.tasks : null
+                      data.previewSearch.objects && data.previewSearch.objects.length > 0 ? Search.objects = data.previewSearch.objects : null
+                      data.previewSearch.users && data.previewSearch.users.length > 0 ? Search.users = data.previewSearch.users : null
 
-                        Search ? console.log("Search", Search) : null
+                      Search ? console.log("Search", Search) : null
 
 
-                        if(Search){
-                          for(let prop in Search) {
-                            console.log("Search." + prop + " = " + Search[prop]);
-                            switch (prop) {
-                              case "tasks":
-                                
-                                break;
-                            
-                              case "objects":
-                                
-                                break;
-                            
-                              case "users":
-                                
-                                break;
-                            
-                              case "messages":
-                                
-                                break;
-                            
-                              default:
-                                break;
-                            }
+                      if(Search){
+                        for(let prop in Search) {
+                          console.log("Search." + prop + " = " + Search[prop]);
+                          switch (prop) {
+                          case "tasks":
+
+                            break;
+
+                          case "objects":
+
+                            break;
+
+                          case "users":
+
+                            break;
+
+                          case "messages":
+
+                            break;
+
+                          default:
+                            break;
                           }
                         }
+                      }
 
                       return(
-                        <div id=""></div>
+                        <div id="">
+                          { Search.messages ? <p>Сообщения</p> : null}
+                          { Search.messages ?
+                            Search.messages.map((e)=>(
+                              <li key={e.id}>{e.text}</li>
+                            )) :  null
+                          }
+                          { Search.tasks ? <p>Задачи</p> : null}
+                          { Search.tasks ?
+                            Search.tasks.map((e)=>(
+                              <li key={e.id}>{e.name}</li>
+                            )) :  null
+                          }
+                          { Search.objects ? <p>Объекты</p> : null}
+                          { Search.objects ?
+                            Search.objects.map((e)=>(
+                              <li key={e.id}>{e.name}</li>
+                            )) :  null
+                          }
+                          { Search.tasks ? <p>Пользователи</p> : null}
+                          { Search.tasks ?
+                            Search.tasks.map((e)=>(
+                              <li key={e.id}>{e.username}</li>
+                            )) :  null
+                          }
+                        </div>
                       )
                     }
-                      return(
-                        "ничего не найдено"
-                      )
+
+                    return(
+                      "ничего не найдено"
+                    )
 
                   }
                 }
