cd331efb6a29961d11b0187c614a8730394bd158
ilnur ibatullin <ilnuribat@gmail.com>
2018-12-03 12:58:51 +0300

    updated migrations

--- server/migrations/20181120T010752-initial.js
+++ server/migrations/20181120T010752-initial.js
@@ -3,7 +3,12 @@ const { Schemas } = require('../src/models');
 
 module.exports = {
   async up() {
-    await Schemas.Files.index({ taskId: 1, fileId: 1 }, { unique: true });
+    await Schemas.Files.index({
+      taskId: 1,
+      fileId: 1,
+    }, {
+      unique: true,
+    });
     await Schemas.Group.index({ code: 1 }, {
       unique: true,
       partialFilterExpression: {
