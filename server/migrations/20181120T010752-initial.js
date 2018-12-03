2a8f65dce32a99331a945c8cbb5448495967adeb
ilnur ibatullin <ilnuribat@gmail.com>
2018-12-03 13:31:48 +0300

    idempotent migrations

--- server/migrations/20181120T010752-initial.js
+++ server/migrations/20181120T010752-initial.js
@@ -2,14 +2,18 @@ const { Schemas } = require('../src/models');
 
 
 module.exports = {
-  async up() {
-    await Schemas.Files.index({
+  async up(client) {
+    await client.collection('files').createIndex({
       taskId: 1,
       fileId: 1,
     }, {
       unique: true,
+      name: 'taskId_fileId',
     });
-    await Schemas.Group.index({ code: 1 }, {
+    await client.collection('groups').createIndex({
+      code: 1,
+    }, {
+      name: 'code_unique',
       unique: true,
       partialFilterExpression: {
         code: {
@@ -17,8 +21,17 @@ module.exports = {
         },
       },
     });
-    await Schemas.User.index({ email: 1 }, { unique: true });
-    await Schemas.UserGroup.index({ userId: 1, groupId: 1 }, { unique: true });
+    await client.collection('users').createIndex({
+      email: 1,
+    }, {
+      unique: true,
+    });
+    await client.collection('usergroups').createIndex({
+      userId: 1,
+      groupId: 1,
+    }, {
+      unique: true,
+    });
   },
   async down() {
     await Schemas.Group.findOne();
