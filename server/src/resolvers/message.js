20eb16539fc24ca2ccbae3f151178b59d0cb5c33
ilnur ibatullin <ilnuribat@gmail.com>
2018-12-11 14:22:16 +0300

    eslint warn

--- server/src/resolvers/message.js
+++ server/src/resolvers/message.js
@@ -3,7 +3,9 @@ const moment = require('moment');
 const {
   Message, User, Group, UserGroup,
 } = require('../models');
-const { MESSAGE_READ, pubsub, MESSAGE_ADDED, ERROR_CODES } = require('../services/constants');
+const {
+  MESSAGE_READ, pubsub, MESSAGE_ADDED, ERROR_CODES,
+} = require('../services/constants');
 
 
 module.exports = {
