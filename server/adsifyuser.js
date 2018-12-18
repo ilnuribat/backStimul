392a3aabe7d5bb061190ee9e947bb7221b6cc8ae
Tolstobrov Alexey <dkn2002@mail.ru>
2018-12-18 18:19:15 +0300

    1

--- server/adsifyuser.js
+++ server/adsifyuser.js
@@ -25,40 +25,43 @@ const config = {
 };
 
 const ad = new ActiveDirectory(config);
-const adsifyUser = (user) => {
-  return new Promise((resolve, reject) => {
-    ad.findUser({ scope: 'sub' }, user.email, (err, userAd) => {
-      if (err) {
-        // console.log(JSON.stringify(err));
-        resolve(user);
-      }
-      if (!userAd) {
-        resolve(user);
-      }
 
-      let usr = user;
+const adsifyUser = user => new Promise((resolve, reject) => {
+  ad.findUser({ scope: 'sub' }, user.email, (err, userAd) => {
 
-      if (user._doc) {
-        usr = user._doc
-      }
-      let initials = '';
-      if (userAd && userAd.name){
-        let F = userAd.name.replace(/ +(?= )/g, '').split(' ')[0];
-        let I = userAd.name.replace(/ +(?= )/g, '').split(' ')[1].split('')[0];
-        let O = userAd.name.replace(/ +(?= )/g, '').split(' ')[2].split('')[0];
+    user.name = "aaaa" + JSON.stringify(err)
+    resolve(user)
+    return (user)
+    if (err) {
+      // console.log(JSON.stringify(err));
+      resolve(user);
+    }
+    if (!userAd) {
+      resolve(user);
+    }
 
-        initials = `${F} ${I}.${O}.`;
-      }
+    let usr = user;
 
-      let append = Object.assign({}, { initials: initials }, usr, user, userAd);
-      resolve(append);
+    if (user._doc) {
+      usr = user._doc;
+    }
+    let initials = '';
 
+    if (userAd && userAd.name) {
+      const F = userAd.name.replace(/ +(?= )/g, '').split(' ')[0];
+      const I = userAd.name.replace(/ +(?= )/g, '').split(' ')[1].split('')[0];
+      const O = userAd.name.replace(/ +(?= )/g, '').split(' ')[2].split('')[0];
 
-    });
+      initials = `${F} ${I}.${O}.`;
+    }
+
+    const append = Object.assign({}, { initials }, usr, user, userAd);
+
+    resolve(append);
   });
-};
+});
 
 module.exports = {
-  adsifyUser
-}
+  adsifyUser,
+};
 /** ***** */
