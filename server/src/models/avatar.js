e301a13975f7044350b1431b43c24a0cd58937e9
ilnur ibatullin <ilnuribat@gmail.com>
2018-12-20 17:13:51 +0300

    use objectId

--- server/src/models/avatar.js
+++ server/src/models/avatar.js
@@ -3,7 +3,7 @@ const { Schema } = require('mongoose');
 const { ObjectId } = Schema.Types;
 
 const schema = new Schema({
-  userId: String,
+  userId: ObjectId,
   content: String,
   name: String,
   email: String,
