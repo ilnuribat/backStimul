118a847850c953f815dfc6b5aaa55423c8c78983
ilnur ibatullin <ilnuribat@gmail.com>
2019-01-09 15:27:27 +0300

    eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "es6": true,
-    "mocha": true
+  extends: 'airbnb-base',
+  env: {
+    es6: true,
+    mocha: true
   },
-  "rules": {
-    "padding-line-between-statements": [
-      "error",
-      { "blankLine": "always", "prev": "*", "next": "return" },
-      { blankLine: "always", prev: ["const", "let", "var"], next: "*"},
-      { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"]}
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', 'prev': '*', 'next': 'return' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*'},
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var']}
     ],
-    "max-len": ["error", 140],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'max-len': ['error', 140],
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    "func-names": ["off"],
-    "class-methods-use-this": ["off"],
-    "no-underscore-dangle": ["error", {"allow": ["_id", "__typename"]}]
+    'func-names': ['off'],
+    'class-methods-use-this': ['off'],
+    'no-underscore-dangle': ['error', {'allow': ['_id', '__typename']}],
+    'no-multiple-empty-lines': ['error', { maxEOF: 1, max: 2 }]
   }
 };
