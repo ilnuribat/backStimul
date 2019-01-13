32b77646a9005f9ce199ae4bc59659b56a8d6e9f
Ilnur Ibatullin <ilnuribat@gmail.com>
2019-01-14 02:45:45 +0300

    test scope secuirity

--- test/direct.js
+++ test/direct.js
@@ -6,7 +6,8 @@ const {
 const { ERROR_CODES } = require('../server/src/services/constants');
 const { generateToken } = require('../server/src/services/user');
 
-describe.only('direct chat', () => {
+describe('direct chat', () => {
+  // создаем tmpUser, tmpUser2, directChat для user и tmpUser2
   before(async function () {
     this.tmpUser = await User.create({
       email: 'tmpDirectUser',
@@ -45,7 +46,7 @@ describe.only('direct chat', () => {
         ],
       },
     });
-    await Group.deleteOne({
+    await Group.deleteMany({
       code: {
         $in: [
           this.directCode,
@@ -53,62 +54,61 @@ describe.only('direct chat', () => {
         ],
       },
     });
-    await Message.deleteMany({
-      groupId: {
-        $in: [
-          this.directChat._id,
-        ],
-      },
-    });
   });
 
-  it('create direct for first time, must be created new group', async function () {
-    const { data, errors } = await this.request({
-      query: `mutation {
-        directMessage(id: "${this.tmpUser._id.toString()}") {
-          id
-          name
-        }
-      }`,
+  describe('smoke suites', () => {
+    // delete created message
+    after(async function () {
+      await Message.deleteMany({ groupId: this.directChat._id });
     });
-
-    assert.isUndefined(errors);
-    const directData = await Group.findById(data.directMessage.id);
-
-    assert.equal(this.directCode, directData.code);
-  });
-  it('direct chat is already exists, try to create one more time - should return same id', async function () {
-    const { data, errors } = await this.request({
-      query: `
-        mutation {
-          directMessage(id: "${this.tmpUser2._id.toString()}") {
+    it('create direct for first time, must be created new group', async function () {
+      const { data, errors } = await this.request({
+        query: `mutation {
+          directMessage(id: "${this.tmpUser._id.toString()}") {
             id
+            name
           }
-        }
-      `,
-    });
+        }`,
+      });
 
-    assert.isUndefined(errors);
-    assert.equal(data.directMessage.id, this.directChat._id.toString());
-  });
-  it('create message in existing direct chat', async function () {
-    const messageText = 'test message';
-    const { data, errors } = await this.request({
-      query: `
-        mutation {
-          message {
-            create(message: { groupId: "${this.directChat._id.toString()}", text: "${messageText}" }) {
+      assert.isUndefined(errors);
+      const directData = await Group.findById(data.directMessage.id);
+
+      assert.equal(this.directCode, directData.code);
+    });
+    it('direct chat is already exists, try to create one more time - should return same id', async function () {
+      const { data, errors } = await this.request({
+        query: `
+          mutation {
+            directMessage(id: "${this.tmpUser2._id.toString()}") {
               id
             }
           }
-        }
-      `,
+        `,
+      });
+
+      assert.isUndefined(errors);
+      assert.equal(data.directMessage.id, this.directChat._id.toString());
     });
+    it('create message in existing direct chat', async function () {
+      const messageText = 'test message';
+      const { data, errors } = await this.request({
+        query: `
+          mutation {
+            message {
+              create(message: { groupId: "${this.directChat._id.toString()}", text: "${messageText}" }) {
+                id
+              }
+            }
+          }
+        `,
+      });
 
-    assert.isUndefined(errors);
-    const message = await Message.findById(data.message.create.id);
+      assert.isUndefined(errors);
+      const message = await Message.findById(data.message.create.id);
 
-    assert.equal(message.text, messageText);
+      assert.equal(message.text, messageText);
+    });
   });
   describe('errors', () => {
     it('try to send message without authorization', async function () {
@@ -182,6 +182,114 @@ describe.only('direct chat', () => {
       assert.isNotEmpty(errors);
       assert.equal(errors[0].message, ERROR_CODES.NOT_FOUND);
     });
+    it('try to create direct chat with non-existing user', async function () {
+      const { data, errors } = await this.request({
+        query: `
+          mutation {
+            directMessage(id: "${ObjectId.createFromTime(Date.now())}") {
+              id
+              name
+            }
+          }
+        `,
+      });
+
+      assert.isNull(data.directMessage);
+      assert.isNotEmpty(errors);
+      assert.equal(errors[0].message, ERROR_CODES.NO_USER_FOUND);
+    });
+  });
+  describe('scope secuirity', () => {
+    // видно двум юзерам в чате и не видно третьему
+    // create two messages to direct chat
+    before(async function () {
+      this.textMessageMainUser = 'test message to direct chat from main user';
+      this.textMessageTmpUser2 = 'test message to direct from tmpUser2';
+      await Message.insertMany([{
+        userId: this.user._id,
+        groupId: this.directChat._id,
+        text: this.textMessageMainUser,
+        isDirect: true,
+      }, {
+        userId: this.tmpUser2._id,
+        groupId: this.directChat._id,
+        text: this.textMessageTmpUser2,
+        isDirect: true,
+      }]);
+    });
+    after(async function () {
+      await Message.deleteMany({
+        groupId: this.directChat._id,
+      });
+    });
+    it('get messages as main user', async function () {
+      const { data, errors } = await this.request({
+        query: `
+        {
+          direct(id: "${this.directChat._id.toString()}") {
+            messages {
+              edges {
+                node {
+                  id
+                  text
+                }
+              }
+            }
+          }
+        }
+        `,
+      });
+
+      assert.isUndefined(errors);
+      assert.equal(data.direct.messages.edges[0].node.text, this.textMessageMainUser);
+      assert.equal(data.direct.messages.edges[1].node.text, this.textMessageTmpUser2);
+    });
+    it('get messages as tmpUser2', async function () {
+      const { data, errors } = await this.request({
+        token: generateToken(this.tmpUser2),
+        query: `
+        {
+          direct(id: "${this.directChat._id.toString()}") {
+            messages {
+              edges {
+                node {
+                  id
+                  text
+                }
+              }
+            }
+          }
+        }
+        `,
+      });
+
+      assert.isUndefined(errors);
+      assert.equal(data.direct.messages.edges[0].node.text, this.textMessageMainUser);
+      assert.equal(data.direct.messages.edges[1].node.text, this.textMessageTmpUser2);
+    });
+    it('try to get messages as another user', async function () {
+      const { data, errors } = await this.request({
+        token: generateToken(this.tmpUser),
+        query: `
+        {
+          direct(id: "${this.directChat._id.toString()}") {
+            messages {
+              edges {
+                node {
+                  id
+                  text
+                }
+              }
+            }
+          }
+        }
+        `,
+      });
+
+      assert.isNotEmpty(errors);
+      assert.equal(errors[0].message, ERROR_CODES.FORBIDDEN);
+      assert.isNull(data.direct);
+    });
   });
 });
 
@@ -191,5 +299,4 @@ describe.only('direct chat', () => {
 // сообщение создается
 
 // сообщение видно другому юзеру
-// lastMessage подгружается
-
+// сообщение не видно третьим лицам
